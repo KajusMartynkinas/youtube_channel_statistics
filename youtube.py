abb3f40804a87aad272dcfa61ba378065320e99e
KajusMartynkinas <61082665+KajusMartynkinas@users.noreply.github.com>
2024-02-04 12:53:04 +0200

    Nearly done

--- youtube.py
+++ youtube.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from googleapiclient.discovery import build
 import pandas as pd
 import seaborn as sns
@@ -34,7 +35,37 @@ def get_channel_stats(youtube, channel_ids):
 channel_statistics = get_channel_stats(youtube, channel_ids)
 
 df = pd.DataFrame(channel_statistics)
+df['Subscribers'] = pd.to_numeric(df['Subscribers'])
+df['Views'] = pd.to_numeric(df['Views'])
+df['Total_videos'] = pd.to_numeric(df['Total_videos'])
 
 print(df)
-
 df.to_csv('channel_statistics.csv')
+
+def subscribers():
+    plt.style.use('dark_background')
+    plt.figure(figsize= (10,8))
+    plt.title('Comparing Subscribers')
+    sns.barplot(x='Channel_name', y = 'Subscribers', data=df, palette='rainbow',
+                order=df.sort_values('Subscribers', ascending=False)['Channel_name'])
+    plt.show()
+
+def total_videos():
+    plt.style.use('dark_background')
+    plt.figure(figsize= (10,8))
+    plt.title('Comparing Total Amount of Videos')
+    sns.barplot(x='Channel_name', y = 'Total_videos', data=df, palette='rainbow',
+                order=df.sort_values('Total_videos', ascending=False)['Channel_name'])
+    plt.show()
+
+def views():
+    plt.style.use('dark_background')
+    plt.figure(figsize= (10,8))
+    plt.title('Comparing Views')
+    sns.barplot(x='Channel_name', y = 'Views', data=df, palette='rainbow',
+                order=df.sort_values('Views', ascending=False)['Channel_name'])
+    plt.show()
+
+subscribers()
+total_videos()
+views()
