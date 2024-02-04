cde6fcbcfd463f71216f8e52b1c577c8a8a2346a
KajusMartynkinas <61082665+KajusMartynkinas@users.noreply.github.com>
2024-02-04 21:24:13 +0200

    Nearly done

--- youtube.py
+++ youtube.py
@@ -3,18 +3,33 @@ from googleapiclient.discovery import build
 import pandas as pd
 import seaborn as sns
 
-api_key = 'AIzaSyCuG4t1hofbFCTEVmjglqrECwmYUTmuSRc'
+
+print('YouTube Channel Comparison')
+print('To continue, enter your API key:')
+
+api_key = input()
+# AIzaSyCuG4t1hofbFCTEVmjglqrECwmYUTmuSRc
+
+id_1 = input('Enter the 1st channel ID: \n')
+id_2 = input('Enter the 2nd channel ID: \n')
+id_3 = input('Enter the 3rd channel ID: \n')
+id_4 = input('Enter the 4th channel ID: \n')
+id_5 = input('Enter the 5th channel ID: \n')
+
 
 channel_id = 'UCq6VFHwMzcMXbuKyG7SQYIg'
-channel_ids = ['UCq6VFHwMzcMXbuKyG7SQYIg',
-               'UCo_IB5145EVNcf8hw1Kku7w',
-               'UC3sznuotAs2ohg_U__Jzj_Q',
-               'UCHYoe8kQ-7Gn9ASOlmI0k6Q',
-               'UCd4t3EEUy0LUOM4MTdjpNHA']
+channel_ids = [id_1,
+               id_2,
+               id_3,
+               id_4,
+               id_5]
+# channel_ids = ['UCq6VFHwMzcMXbuKyG7SQYIg',
+#                'UCo_IB5145EVNcf8hw1Kku7w',
+#                'UC3sznuotAs2ohg_U__Jzj_Q',
+#                'UCHYoe8kQ-7Gn9ASOlmI0k6Q',
+#                'UCd4t3EEUy0LUOM4MTdjpNHA']
 
 youtube = build('youtube','v3', developerKey = api_key)
-
-# Function to get channel statistics
 
 def get_channel_stats(youtube, channel_ids):
     all_data = []
@@ -38,6 +53,8 @@ df = pd.DataFrame(channel_statistics)
 df['Subscribers'] = pd.to_numeric(df['Subscribers'])
 df['Views'] = pd.to_numeric(df['Views'])
 df['Total_videos'] = pd.to_numeric(df['Total_videos'])
+df['Views_per_video'] = df['Views']/df['Total_videos']
+
 
 print(df)
 df.to_csv('channel_statistics.csv')
@@ -66,6 +83,15 @@ def views():
                 order=df.sort_values('Views', ascending=False)['Channel_name'])
     plt.show()
 
+def views_per_video():
+    plt.style.use('dark_background')
+    plt.figure(figsize=(10, 8))
+    plt.title('Views per video')
+    sns.barplot(x='Channel_name', y='Views_per_video', data=df, palette='rainbow',
+                order=df.sort_values('Views_per_video', ascending=False)['Channel_name'])
+    plt.show()
+
 subscribers()
 total_videos()
 views()
+views_per_video()
