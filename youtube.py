59e27c0d6b566bd462544b19357d59f6097135ef
KajusMartynkinas <61082665+KajusMartynkinas@users.noreply.github.com>
2024-02-05 10:57:08 +0200

    Done

--- youtube.py
+++ youtube.py
@@ -8,26 +8,17 @@ print('YouTube Channel Comparison')
 print('To continue, enter your API key:')
 
 api_key = input()
-# AIzaSyCuG4t1hofbFCTEVmjglqrECwmYUTmuSRc
 
-id_1 = input('Enter the 1st channel ID: \n')
-id_2 = input('Enter the 2nd channel ID: \n')
-id_3 = input('Enter the 3rd channel ID: \n')
-id_4 = input('Enter the 4th channel ID: \n')
-id_5 = input('Enter the 5th channel ID: \n')
+channel_ids = []
+skip = False
 
-
-channel_id = 'UCq6VFHwMzcMXbuKyG7SQYIg'
-channel_ids = [id_1,
-               id_2,
-               id_3,
-               id_4,
-               id_5]
-# channel_ids = ['UCq6VFHwMzcMXbuKyG7SQYIg',
-#                'UCo_IB5145EVNcf8hw1Kku7w',
-#                'UC3sznuotAs2ohg_U__Jzj_Q',
-#                'UCHYoe8kQ-7Gn9ASOlmI0k6Q',
-#                'UCd4t3EEUy0LUOM4MTdjpNHA']
+for i in range(1, 11):
+    if not skip:
+        channel_id = input(f'Enter the {i}th channel ID (or write "skip" to end): \n')
+        if channel_id.lower() == 'skip':
+            skip = True
+        else:
+            channel_ids.append(channel_id)
 
 youtube = build('youtube','v3', developerKey = api_key)
 
@@ -61,7 +52,7 @@ df.to_csv('channel_statistics.csv')
 
 def subscribers():
     plt.style.use('dark_background')
-    plt.figure(figsize= (10,8))
+    plt.figure(figsize= (14,9))
     plt.title('Comparing Subscribers')
     sns.barplot(x='Channel_name', y = 'Subscribers', data=df, palette='rainbow',
                 order=df.sort_values('Subscribers', ascending=False)['Channel_name'])
@@ -69,7 +60,7 @@ def subscribers():
 
 def total_videos():
     plt.style.use('dark_background')
-    plt.figure(figsize= (10,8))
+    plt.figure(figsize= (14,9))
     plt.title('Comparing Total Amount of Videos')
     sns.barplot(x='Channel_name', y = 'Total_videos', data=df, palette='rainbow',
                 order=df.sort_values('Total_videos', ascending=False)['Channel_name'])
@@ -77,7 +68,7 @@ def total_videos():
 
 def views():
     plt.style.use('dark_background')
-    plt.figure(figsize= (10,8))
+    plt.figure(figsize= (14,9))
     plt.title('Comparing Views')
     sns.barplot(x='Channel_name', y = 'Views', data=df, palette='rainbow',
                 order=df.sort_values('Views', ascending=False)['Channel_name'])
@@ -85,7 +76,7 @@ def views():
 
 def views_per_video():
     plt.style.use('dark_background')
-    plt.figure(figsize=(10, 8))
+    plt.figure(figsize=(14, 9))
     plt.title('Views per video')
     sns.barplot(x='Channel_name', y='Views_per_video', data=df, palette='rainbow',
                 order=df.sort_values('Views_per_video', ascending=False)['Channel_name'])
